f04fb4e0c8d001b326851fdb28b1f92b2549cfc5
olayeancarh <akejuolayinka016@gmail.com>
2019-08-23 23:41:06 +0100

    Updates

---
+++ precache-manifest.53d1615904b315f7d13167aad9d8d584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c746fd5c5577f618e4c34b2ed585a88",
+    "url": "/react-videos-app/index.html"
+  },
+  {
+    "revision": "c0b8bb79d2ac2f4a3c44",
+    "url": "/react-videos-app/static/css/main.e0a37180.chunk.css"
+  },
+  {
+    "revision": "24311859829b29ef2245",
+    "url": "/react-videos-app/static/js/2.f6cf09dd.chunk.js"
+  },
+  {
+    "revision": "c0b8bb79d2ac2f4a3c44",
+    "url": "/react-videos-app/static/js/main.d647ebed.chunk.js"
+  },
+  {
+    "revision": "20779aa8ff223d7f802f",
+    "url": "/react-videos-app/static/js/runtime~main.d68c472a.js"
+  }
+]);
